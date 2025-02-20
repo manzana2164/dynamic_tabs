6ae5fd9b09e93f88701abbc48062f1dbdfac0f34
manzana2164 <emiliopascutti2164@gmail.com>
2025-02-20 18:18:58 -0300

    change files to portfolio layout

--- scripts.js
+++ scripts.js
@@ -1,24 +1,85 @@
 // scripts.js
 jQuery(document).ready(function($) {
-    $('.tab-link').on('click', function() {
-        var tab = $(this).data('tab');
-
-        // Remove active classes
-        $('.tab-link').removeClass('active');
-        $('.tab-content').removeClass('active');
-        $('body').removeClass('bg-arquitectura bg-ingenieria bg-construccion');
-
-        // Activate the selected tab
+    // Toggle view
+    $('.view-toggle').on('click', function() {
+        var view = $(this).data('view');
+        $('.view-toggle').removeClass('active');
         $(this).addClass('active');
-        $('#' + tab).addClass('active');
+        $('.portfolio-posts').removeClass('grid-view list-view').addClass(view + '-view');
+    });
 
-        // Change background based on the tab
-        if (tab === 'tab1') {
-            $('body').addClass('bg-arquitectura');
-        } else if (tab === 'tab2') {
-            $('body').addClass('bg-ingenieria');
-        } else if (tab === 'tab3') {
-            $('body').addClass('bg-construccion');
+    // Filter posts
+    $('#category-filter, #year-filter, #place-filter').on('change', function() {
+        filterPosts();
+    });
+
+    // Sort posts A-Z
+    $('#sort-az').on('click', function() {
+        sortPostsAZ();
+    });
+
+    function filterPosts() {
+        var category = $('#category-filter').val();
+        var year = $('#year-filter').val();
+        var place = $('#place-filter').val();
+
+        console.log('Filtering posts with:', { category, year, place });
+
+        $('.post').each(function() {
+            var postCategory = $(this).data('category');
+            var postYear = $(this).data('year');
+            var postPlace = $(this).data('place');
+
+            console.log('Post data:', { postCategory, postYear, postPlace });
+
+            if ((category === '' || category === postCategory) &&
+                (year === '' || year == postYear) && // Ensure year comparison is correct
+                (place === '' || place === postPlace)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
+    function sortPostsAZ() {
+        var posts = $('.post').get();
+        posts.sort(function(a, b) {
+            var titleA = $(a).find('.post-title').text().toUpperCase();
+            var titleB = $(b).find('.post-title').text().toUpperCase();
+            return (titleA < titleB) ? -1 : (titleA > titleB) ? 1 : 0;
+        });
+        $.each(posts, function(idx, post) {
+            $('.portfolio-posts').append(post);
+        });
+    }
+
+    // Pagination
+    var itemsPerPage = 6;
+    var currentPage = 1;
+
+    function showPage(page) {
+        var start = (page - 1) * itemsPerPage;
+        var end = start + itemsPerPage;
+
+        $('.post').hide().slice(start, end).show();
+        $('#page-number').text(page);
+    }
+
+    $('#prev-page').on('click', function() {
+        if (currentPage > 1) {
+            currentPage--;
+            showPage(currentPage);
         }
     });
+
+    $('#next-page').on('click', function() {
+        if (currentPage * itemsPerPage < $('.post').length) {
+            currentPage++;
+            showPage(currentPage);
+        }
+    });
+
+    // Initial display
+    showPage(currentPage);
 });
